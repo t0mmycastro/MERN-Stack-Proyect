e893d363e2d2ba5345046a85a1bfd37444d8dbf1
Tomás <ttomycastro@gmail.com>
2023-02-22 01:39:43 -0300

    termine por hoy xd

--- client/src/pages/auth/Welcome.jsx
+++ client/src/pages/auth/Welcome.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Welcome = () => {
+
+    const { username, isManager, isAdmin } = useAuth()
+    // extraemos username, ismanager e isadmin
 
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
@@ -10,15 +14,16 @@ const Welcome = () => {
 
             <p>{today}</p>
 
-            <h1>Bienvenido!</h1>
+            <h1>Bienvenido {username}!</h1>
 
             <p><Link to="/dash/notes">Ver las notas</Link></p>
 
             <p><Link to="/dash/notes/new">Añadir nuevas notas</Link></p>
 
-            <p><Link to="/dash/users">Ver usuarios y configuracion</Link></p>
+            {/* Si tenemos un manager o un administrador, si algunos de estos es true, se mostrará el ver usuarios y configuracion */}
+            {(isManager || isAdmin) && <p><Link to="/dash/users">Ver usuarios y configuracion</Link></p>}
 
-            <p><Link to="/dash/users/new">Añadir nuevo usuario</Link></p>
+            {(isManager || isAdmin) &&<p><Link to="/dash/users/new">Añadir nuevo usuario</Link></p>}
 
         </section>
     )
